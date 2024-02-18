9cd50ea6328d74d1e67b3c49ee7d76cc92f32dd1
Ian Pares <yannpares@gmail.com>
2024-02-18 18:26:12 +0100

    (add) First commit. Basic structure and some logos and links

---
+++ shakeItUp/shake-it-app/src/pages/mainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchBar from '../../components/searchBar/SearchBar'
+
+type Props = {}
+
+const MainPage = (props: Props) => {
+  return (
+    
+    <SearchBar/>
+  )
+}
+
+export default MainPage
